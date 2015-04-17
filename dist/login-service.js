c0fb451c2354e4ba1b8a8fe862fca5d8e8a79526
Remy Sharp <remy@remysharp.com>
2015-04-17 13:51:49 +0100

    0.1.0

---
+++ dist/login-service.js
@@ -0,0 +1,155 @@
+;(function() {
+  'use strict';
+
+  /* globals prompt*/ // nasty
+
+  var ngModule = angular.module('eha.login-service', ['pouchdb'])
+  .value('localforage', window.localforage)
+  .provider('ehaLoginService', function() {
+    var DB_NAME = null;
+    this.config = function(database) {
+      DB_NAME = database;
+    };
+
+    this.$get = ['$q', 'localforage', 'pouchDB', function(
+      $q,
+      localforage,
+      pouchDB
+    ) {
+      var loginService = this;
+
+      // notificationService is a promise that must resolve with an array
+      // containing the username & password.
+      var notificationService = function() {
+        var username = prompt('Username?');
+        var password = prompt('Password?');
+        return $q.when([username, password]);
+      };
+
+      loginService.config = function(notifier) {
+        if (typeof notifier !== 'function') {
+          throw new Error('notification service must be a function that ' +
+            'returns a promise');
+        }
+        notificationService = notifier;
+      };
+
+      var _db; // cached db connection
+      var db = function() {
+        if (!DB_NAME) {
+          throw new Error('loginService must be configured with ' +
+            ' .config({ database: "<url/db_name to couchdb>"');
+        }
+
+        _db = _db || pouchDB(DB_NAME);
+        return _db;
+      };
+
+      function localforgeWrap(method) {
+        return function() {
+          var deferred = $q.defer();
+          var args = [].slice.call(arguments, 0);
+
+          args.push(function(error, value) {
+            if (error) {
+              deferred.reject(error);
+            } else {
+              deferred.resolve(value);
+            }
+          });
+
+          localforage[method].apply(localforage, args);
+
+          return deferred.promise;
+        };
+      }
+
+      var setItem = localforgeWrap('setItem');
+      var getItem = localforgeWrap('getItem');
+      var removeItem = localforgeWrap('removeItem');
+
+      var storeCredentials = function(username, password) {
+        var promises = [
+          setItem('username', username),
+          setItem('password', password),
+        ];
+
+        return $q.all(promises);
+      };
+
+      var getUserPass = function() {
+        return $q.all([
+          getItem('username'),
+          getItem('password')
+        ]);
+      };
+
+      var hasDatabaseCredentials = function() {
+        return getUserPass().then(function(creds) {
+          // Couch requires both username and password
+          // Otherwise it'll return a 400 Bad Request
+          return !!(creds[0] && creds[1]);
+        });
+      };
+
+      loginService.getUserName = getItem.bind(null, 'username');
+
+      loginService.maybeShowLoginUi = function() {
+        return hasDatabaseCredentials().then(function(has) {
+          if (has) {
+            return getUserPass();
+          } else {
+            return notificationService().then(function(creds) {
+                // $q promise can only take one value
+                return storeCredentials.apply(null, creds);
+              })
+              .then(getUserPass);
+          }
+        });
+      };
+
+      loginService.hasLocalCreds = function() {
+        return hasDatabaseCredentials();
+      };
+
+      loginService.login = function(username, password) {
+        var store = storeCredentials.bind(null, username, password);
+        return db().login(username, password).then(store);
+      };
+
+      loginService.renew = function() {
+        return hasDatabaseCredentials().then(function(has) {
+          if (has) {
+            return getUserPass().then(function(res) {
+              return loginService.login(res[0], res[1]);
+            });
+          }
+
+          // Check the retriable service if changing this
+          return $q.reject({status: 401, message: 'credentials not found'});
+        });
+      };
+
+      // FIXME this could mess things up https://github.com/eHealthAfrica/BiometricRegistration/blob/f1492732380322aca7415defd7dcb222034750f2/app/scripts/services/logout.js#L5
+      loginService.logout = function() {
+        return $q.all([
+          removeItem('username'),
+          removeItem('password'),
+        ]);
+      };
+
+      loginService.storeCredentials = function(username, password) {
+        return storeCredentials(username, password);
+      };
+
+      return loginService;
+
+    }];
+  });
+
+  // Check for and export to commonjs environment
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = ngModule;
+  }
+
+})();
---
+++ tests/login-service.spec.js
@@ -0,0 +1,219 @@
+(function() {
+  /*global localforage*/
+  'use strict';
+
+  function digest() {
+    inject(function($rootScope) {
+      $rootScope.$digest();
+    });
+  }
+
+  function digestIt(original) {
+    return function(description, testFn) {
+      var isAsync = testFn.length >= 1;
+      var fn = !isAsync ? testFn : function(done) {
+        testFn(done);
+        digest();
+      };
+      original(description, fn);
+    };
+  }
+
+  // test helper/override thing
+  it = digestIt(it); // jshint ignore:line
+
+  // Angular modules needed
+  var loginService;
+  var $q;
+  var $rootScope;
+  // Fake local store
+  // var localforage;
+  // Fake local pouch
+  var pouchSpy;
+  var localFake = {};
+  var ehaLoginServiceProvider;
+
+  beforeEach(module(function($provide) {
+    // Fake out the little pouch we use in login service
+    pouchSpy = sinon.spy(function(user, pass) {
+      if (user && pass) {
+        return $q.when({'ok':true, 'name': user, 'roles':[]});
+      }
+      return $q.reject({status: 401, message: 'Name or password invalid'});
+    });
+
+    angular.module('pouchdb', function() {});
+    $provide.value('pouchDB', sinon.spy(function() {
+      return {
+        login: pouchSpy
+      };
+    }));
+  }));
+
+  beforeEach(function(done) {
+    try {
+      localforage.setItem.restore();
+      localforage.getItem.restore();
+      localforage.removeItem.restore();
+    } catch (e) {
+      // it didn't run yet.
+    }
+
+    localFake = {};
+    sinon.stub(localforage, 'setItem', function(key, value, callback) {
+      localFake[key] = value;
+
+      if (callback) {
+        callback(null);
+      }
+
+      return Promise.resolve();
+    });
+
+    sinon.stub(localforage, 'getItem', function(key, callback) {
+      if (callback) {
+        callback(null, localFake[key]);
+      }
+      return Promise.resolve(localFake[key]);
+    });
+
+    sinon.stub(localforage, 'removeItem', function(key, callback) {
+      delete localFake[key];
+      if (callback) {
+        callback(null);
+      }
+      return Promise.resolve();
+    });
+
+    // finally, reset the localforge
+    localforage.clear(function() {
+      done();
+    });
+  });
+
+  beforeEach(function() {
+    angular.module('eha.login-service.test', function() {
+    }).config(function(_ehaLoginServiceProvider_) {
+      ehaLoginServiceProvider = _ehaLoginServiceProvider_;
+      _ehaLoginServiceProvider_.config('my_db_url');
+    });
+
+    // I don't quite understand why this works, but I need it to do config
+    // magic.
+    module('eha.login-service', 'eha.login-service.test');
+
+    inject(function(_$q_, _ehaLoginService_, _$rootScope_) {
+      $q = _$q_;
+      loginService = _ehaLoginService_;
+      $rootScope = _$rootScope_;
+    });
+  });
+
+  describe('Service: loginService', function() {
+    it('should store credentials', function(done) {
+      loginService.storeCredentials('karl', 'pineapple')
+        .then(function() {
+          expect(localforage.setItem.calledOnce);
+          expect(localforage.setItem.withArgs('username', 'karl').calledOnce);
+          expect(localforage.setItem.withArgs('password', 'pineapple').calledOnce);
+          done();
+        });
+    });
+
+    it('should report valid creds only if both username and pass is there', function(done) {
+      // default: has nothing
+      new Promise(function(resolve) {
+        loginService.hasLocalCreds().then(function(hasCreds) {
+          expect(hasCreds).to.equal(false);
+          resolve();
+        });
+      }).then(function() {
+        return localforage.setItem('username', 'myuser', function() {
+          loginService.hasLocalCreds().then(function(hasCreds) {
+            expect(hasCreds).to.equal(false);
+          });
+          digest();
+        });
+      }).then(function() {
+        return Promise.all([
+          localforage.setItem('username', 'myuser'),
+          localforage.setItem('password', 'mypass')
+        ]).then(function() {
+          loginService.hasLocalCreds().then(function(hasCreds) {
+            expect(hasCreds).to.equal(true);
+            done();
+          });
+          digest();
+        });
+      }).catch(function(error) {
+        console.log(error.message);
+        done(error);
+      });
+    });
+
+    it('should delete creds when logging out', function(done) {
+      Promise.all([
+        localforage.setItem('username', 'nicklas'),
+        localforage.setItem('password', 'backstrom'),
+      ]).then(function() {
+        loginService.logout().then(function() {
+          expect(localFake.username).to.be.undefined;
+          expect(localFake.password).to.be.undefined;
+          expect(localforage.removeItem.called);
+        });
+        digest();
+      }).then(done).catch(done);
+    });
+
+    it('should renew the session if it has creds', function(done) {
+      localFake.username = 'santa';
+      localFake.password = 'claus';
+
+      loginService.renew().then(function() {
+        expect(pouchSpy.called);
+        var logins = pouchSpy.lastCall;
+        expect(pouchSpy).to.have.been.calledWith('santa', 'claus');
+        expect(localFake.username).to.equal('santa');
+        expect(localFake.password).to.equal('claus');
+        done();
+      });
+    });
+
+    it('should not renew session if it doesn’t have creds', function(done) {
+      localFake.username = 'santa';
+      localFake.password = undefined; // bam bam bAAAAAAM
+
+      loginService.renew().catch(function(err) {
+        expect(pouchSpy.callCount === 0);
+
+        // Make it return 401 right now, works with the retrial lopp
+        expect(err.status).to.equal(401);
+        done();
+      });
+    });
+
+    it('should prompt user for creds if it doesn’t have any', function(done) {
+      ehaLoginServiceProvider.config(function() {
+        var defer = $q.defer();
+
+        setTimeout(function() {
+          defer.resolve(['remy', 'password']);
+          digest();
+        }, 100);
+
+        return defer.promise;
+      });
+
+      inject(function(ehaLoginService) {
+        ehaLoginService.maybeShowLoginUi().then(function(creds) {
+          expect(creds).deep.equal(['remy', 'password']);
+          ehaLoginService.maybeShowLoginUi().then(function(creds) {
+            expect(creds).deep.equal(['remy', 'password']);
+            done();
+          });
+        });
+      });
+    });
+  });
+
+})();
